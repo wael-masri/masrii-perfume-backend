ba9eae1ab6e71643d1cadc33e608747d4afd8bd3
wael masri <masri_1997@hotmail.com>
2023-05-02 14:13:08 +0300

    first commit

--- client/src/Redux/Reducers/Typescart.js
+++
@@ -1,5 +0,0 @@
-// type of actions
-export const add_to_cart = "ADDTOCART";
-export const delete_from_cart = "DELETEFROMCART";
-export const clear_all_cart = "CLEARALLCART";
-export const modify_cart = "MODIFYCART";
